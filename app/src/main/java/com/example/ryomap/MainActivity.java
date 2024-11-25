8dcc982b09fb00773ca9b04c2729e3e40698460c
Mitsui1 <260367p@ugs.kochi-tech.ac.jp>
2024-11-25 19:03:55 +0900

    Changes

--- app/src/main/java/com/example/ryomap/MainActivity.java
+++ app/src/main/java/com/example/ryomap/MainActivity.java
@@ -1,26 +1,25 @@
 package com.example.ryomap;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button button_login = findViewById(R.id.button_login); //画面遷移先のトリガーとなるボタン指定
+        button_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                overridePendingTransition(0, 0); //画面遷移のアニメーション削除
+            }
         });
-
-
     }
 }
